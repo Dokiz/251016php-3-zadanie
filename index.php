ae0d086b37a41ab5171587d8781b5dd49562eeb3
Dokiz_228@mail.ru <deoptgqqr12>
2016-10-25 16:26:29 +0300

    start

--- index.php
+++ index.php
@@ -2,9 +2,13 @@
 require_once 'ConcreteClass2.php';
 $class1 = new ConcreteClass1;
 $class1->printOut();
+echo "</br>";
 echo $class1->prefixValue('FOO_') ."\n";
+echo "</br>";
 
 $class2 = new ConcreteClass2;
 $class2->printOut();
+echo "</br>";
 echo $class2->prefixValue('FOO_') ."\n";
+echo "</br>";
 ?>
